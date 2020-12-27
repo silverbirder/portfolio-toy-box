f4095844e7b293a1afe67ccb78f2a92c9d0d495d
silverbirder <silverbirder@gmail.com>
2020-12-27 22:24:53 +0000

    😎 add md rendering

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import MyElement from './assets/js/myElement.js'
+import MyElement from './assets/js/components/myElement.js'
 
 customElements.define("my-element", MyElement)
+
+// window["markdown"].ready.then(async (markdown) => {
+//     const md = await (await fetch('./assets/md/sample.md')).text();
+//     document.querySelector('body').innerHTML = markdown.parse(md);
+// })
