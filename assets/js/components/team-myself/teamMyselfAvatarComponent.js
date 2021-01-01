aee5a858ada9085f76ba24b0d3376e9c397c9164
shibamoto <silverbirder@gmail.com>
2021-01-02 00:10:04 +0900

    😎 Try sample code

--- assets/js/components/team-myself/teamMyselfAvatarComponent.js
+++ assets/js/components/team-myself/teamMyselfAvatarComponent.js
@@ -2,6 +2,6 @@ import { LitElement, html } from "https://unpkg.com/lit-element/lit-element.js?m
 
 export default class TeamMyselfAvatarComponent extends LitElement {
     render(){
-        return html`TeamMyselfAvatarComponent`
+        return html`<img src="https://res.cloudinary.com/silverbirder/image/upload/ar_1:1,b_rgb:ffffff,bo_0px_solid_rgb:ffffff,c_fill,g_auto,o_100,r_max,w_180/v1609512845/github/silver-birder/IMG_3040.jpg">`
     }
 }
--- assets/js/components/team-myself/teamMyselfCareerComponent.js
+++ assets/js/components/team-myself/teamMyselfCareerComponent.js
@@ -2,6 +2,21 @@ import { LitElement, html } from "https://unpkg.com/lit-element/lit-element.js?m
 
 export default class TeamMyselfCareerComponent extends LitElement {
     render(){
-        return html`TeamMyselfCareerComponent`
+        return html`
+        <table>
+          <thead>
+            <tr>
+              <th>header1</th>
+              <th>header2</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>you1</td>
+              <td>you2</td>
+            </tr>
+          </tbody>
+        </table>
+        `
     }
 }
