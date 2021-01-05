e63a856f24b874000316c17d91fc3622fe3fd6d6
shibamoto <silverbirder@gmail.com>
2021-01-05 22:52:40 +0900

    ✍️ Update certification and accomplishment

--- assets/js/components/team-ability/teamAbilityCertificationComponent.js
+++ assets/js/components/team-ability/teamAbilityCertificationComponent.js
@@ -5,7 +5,25 @@ export default class TeamAbilityCertificationComponent extends BaseComponent {
     render(){
         return html`
         ${super.preRender()}
-        TeamAbilityCertificationComponent
+        <h3>Certification</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Summary</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2015/04</td>
+              <td>💯 Passing IPA:Information-technology Promotion Agency, Japan</td>
+            </tr>
+            <tr>
+              <td>2017/09</td>
+              <td>💯 Passing Java SE 8, Oracle</td>
+            </tr>
+          </tbody>
+        </table>
         `
     }
 }
