260431fbd017ca2843a9932d93c8ca34ecd2553c
shibamoto <silverbirder@gmail.com>
2021-01-05 22:50:34 +0900

    😎 update base files

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 import TeamAbilityAccomplishmentComponent from './assets/js/components/team-ability/teamAbilityAccomplishmentComponent.js'
 import TeamAbilityCertificationComponent from './assets/js/components/team-ability/teamAbilityCertificationComponent.js'
-import TeamAbilityJobComponent from './assets/js/components/team-ability/teamAbilityJobComponent.js'
 import TeamAbilitySkillComponent from './assets/js/components/team-ability/teamAbilitySkillComponent.js'
-import TeamCreationBlogComponent from './assets/js/components/team-creation/teamCreationBlogComponent.js'
-import TeamCreationProjectComponent from './assets/js/components/team-creation/teamCreationProjectComponent.js'
 import TeamMyselfAvatarComponent from './assets/js/components/team-myself/teamMyselfAvatarComponent.js'
 import TeamMyselfBiographyComponent from './assets/js/components/team-myself/teamMyselfBiographyComponent.js'
 import TeamMyselfCareerComponent from './assets/js/components/team-myself/teamMyselfCareerComponent.js'
 import TeamMyselfInterestComponent from './assets/js/components/team-myself/teamMyselfInterestComponent.js'
-import TeamMyselfLinksComponent from './assets/js/components/team-myself/teamMyselfLinksComponent.js'
+import TeamMyselfLinkComponent from './assets/js/components/team-myself/teamMyselfLinkComponent.js'
 
 customElements.define('team-ability-accomplishment', TeamAbilityAccomplishmentComponent)
 customElements.define('team-ability-certification', TeamAbilityCertificationComponent)
-customElements.define('team-ability-job', TeamAbilityJobComponent)
 customElements.define('team-ability-skill', TeamAbilitySkillComponent)
-customElements.define('team-creation-blog', TeamCreationBlogComponent)
-customElements.define('team-creation-project', TeamCreationProjectComponent)
 customElements.define('team-myself-avatar', TeamMyselfAvatarComponent)
 customElements.define('team-myself-biography', TeamMyselfBiographyComponent)
 customElements.define('team-myself-career', TeamMyselfCareerComponent)
 customElements.define('team-myself-interest', TeamMyselfInterestComponent)
-customElements.define('team-myself-links', TeamMyselfLinksComponent)
+customElements.define('team-myself-link', TeamMyselfLinkComponent)
