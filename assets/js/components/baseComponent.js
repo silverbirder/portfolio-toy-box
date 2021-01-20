960d8c7d2d66964ae76b85e2accd94c092391fbc
shibamoto <silverbirder@gmail.com>
2021-01-20 21:51:37 +0900

    🛠 Use gulp-sample folder

--- assets/js/components/baseComponent.js
+++
@@ -1,7 +0,0 @@
-import { LitElement, html } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module"
-
-export default class BaseComponent extends LitElement {
-    preRender(){
-        return html`<link href="../../../index.css" rel="stylesheet" type="text/css">`
-    }
-}
--- gulp-sample/assets/script.js
+++
@@ -1,3 +0,0 @@
-const p = document.createElement('p');
-p.textContent = 'A second line of text';
-document.body.appendChild(p);
