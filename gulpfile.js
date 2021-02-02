2d69d581cf8dad9cd115091e75e0202950cb4678
shibamoto <silverbirder@gmail.com>
2021-02-02 21:29:37 +0900

    🙂 Add amp-list from json

--- gulpfile.js
+++ gulpfile.js
@@ -114,21 +114,52 @@ const buildAggregatedJSONPipeline = async (file, enc, cb) => {
     const filePath = file.history[0];
     const jsonDir = path.dirname(`${filePath}`);
     const jsonFile = JSON.parse(file.contents.toString());
-    const jsonList = await streamToString(
+    const jsonList = (await streamToString(
         await src(path.join(jsonDir, jsonFile.source))
             .pipe(through.obj(extractMarkdownToJSONPipeline))
-    );
+    )).map((markdownJson) => {
+        return JSON.parse(markdownJson.contents.toString());
+    }).map((markdownJson) => {
+        markdownJson['draft'] = markdownJson['draft'] === 'true';
+        const d = new Date(markdownJson['date']);
+        markdownJson['date'] = d;
+        markdownJson['humanDate'] = `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`;
+        return markdownJson;
+    }).filter((markdownJson) => {
+        return markdownJson.draft === false
+    }).sort((a, b) => {
+        if (a.date > b.date) {
+            return -1
+        } else if (a.date < b.date) {
+            return 1
+        } else {
+            return 0
+        }
+    });
+    const fileJson = JSON.parse(file.contents.toString());
+    fileJson.items = jsonList;
+    file.contents = Buffer.from(JSON.stringify(fileJson));
     cb(null, file)
 };
 
 const extractMarkdownToJSONPipeline = async (file, enc, cb) => {
+    const path = require('path');
     const content = file.contents.toString();
+    const filePath = file.history[0];
     const headRegex = new RegExp("<!--(?<content>((?!-->).*\n)*?)-->");
     const head = content.match(headRegex).groups.content.trim();
+    const splitRegex = new RegExp("(?<=^[^:]+?):");
+    const responseJson = {};
     head.split('\n').map((rows) => {
-
+        const [key, value] = rows.split(splitRegex);
+        responseJson[key.trim()] = value.trim();
     });
-    file.contents = Buffer.from(JSON.stringify({a: 1}));
+    const contentPath = path
+        .relative(__dirname, filePath)
+        .replace(/^src\//, '')
+        .replace(/\.md$/, '.html');
+    responseJson['url'] = `${BASE_URL}/${contentPath}`;
+    file.contents = Buffer.from(JSON.stringify(responseJson));
     cb(null, file)
 };
 
