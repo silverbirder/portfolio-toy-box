07a081bae1ec6d79d9fc78dc553ea5a731e826b0
shibamoto <silverbirder@gmail.com>
2021-02-03 21:33:22 +0900

    😎 Add title and favicon

--- gulpfile.js
+++ gulpfile.js
@@ -101,8 +101,12 @@ const buildMarkdownPipeline = async (file, enc, cb) => {
     const filePath = file.history[0];
     const canonicalUrl = generateCanonicalUrl(filePath).replace(/\.md$/, '');
     const layout = fs.readFileSync('./templates/layout.html', 'utf-8');
+    const cloneFile = Object.create(file);
+    await extractMarkdownToJSONPipeline(cloneFile, enc, (_1, _2) => {});
+    const markdownJson = JSON.parse(cloneFile.contents.toString());
+    const headHtml = `<head>\n<link rel="canonical" href="${canonicalUrl}">\n<title>${markdownJson.title} - silverbirder's page</title>\n<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>✍</text></svg>">\n</head>\n`;
     const html = await optimizeAMP(
-        buildHTML(`<head>\n<link rel="canonical" href="${canonicalUrl}">\n</head>\n<main>\n${markdownHtml}\n</main>`, layout), {
+        buildHTML(`${headHtml}<main>\n${markdownHtml}\n</main>`, layout), {
             markdown: true
         });
     file.contents = Buffer.from(html);
@@ -153,7 +157,6 @@ const buildAggregatedJSONPipeline = async (file, enc, cb) => {
 };
 
 const extractMarkdownToJSONPipeline = async (file, enc, cb) => {
-    const path = require('path');
     const content = file.contents.toString();
     const filePath = file.history[0];
     const headRegex = new RegExp("<!--(?<content>((?!-->).*\n)*?)-->");
